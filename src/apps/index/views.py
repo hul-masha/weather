3f17fa3a02c1897695f9d119d480a08fc59f9d1f
Gul-lug <masha.gul15@gmail.com>
2020-06-13 05:12:32 +0300

    compare_data_formating

--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -1,5 +1,5 @@
-import json
 import datetime
+import json
 
 from django.urls import reverse_lazy
 from django.views import View
@@ -14,6 +14,7 @@ from apps.index.models import Weather
 api_city = "London"
 nw = datetime.datetime.now()
 
+
 class IndexView(ListView):
 
     template_name = "index/index.html"
@@ -25,18 +26,18 @@ class IndexView(ListView):
 
     def get_context_data(self, **kwargs):
         parent_ctx = super().get_context_data()
-        k=same_data(nw, api_city)
+        k = same_data(nw, api_city)
         if k:
             ctx = {"w": [api_city, k.we, str(k.data)]}
         else:
             import requests
 
             r = requests.get(
-            #'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b13bc69dfb1da2ace7b8a62928fef4f0')
-            f"http://api.openweathermap.org/data/2.5/weather?q={api_city}&APPID={settings.API_KEY}"
+                #'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b13bc69dfb1da2ace7b8a62928fef4f0')
+                f"http://api.openweathermap.org/data/2.5/weather?q={api_city}&APPID={settings.API_KEY}"
             )
             payload = json.loads(r.text)
-        # print(payload)
+            # print(payload)
             weather = payload["main"]["temp"]  # - 273.15
             we_с = weather - 273
             place = payload["name"]
@@ -61,13 +62,17 @@ class UpView(FormView):
         print(c)
         global api_city, nw
         api_city = c
-        nw=form.cleaned_data.get("dat")
+        nw = form.cleaned_data.get("dat")
         print(nw)
         return super().form_valid(form)
 
+
 def same_data(now_dat, town):
-    for p in Weather.objects.all():
-        if p.data.strftime("%d-%m-%Y %H") == now_dat.strftime("%d-%m-%Y %H"):
-            if p.city == town:
-                return p
-    return False
+    try:
+        for p in Weather.objects.all():
+            if p.data.strftime("%d-%m-%Y %H") == now_dat.strftime("%d-%m-%Y %H"):
+                if p.city == town:
+                    return p
+        return False
+    except Exception:
+        return False
